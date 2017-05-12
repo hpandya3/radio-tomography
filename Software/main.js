e1e73e3c0020e3f10271988ece68278277b299c1
harshp <hpand3@gmail.com>
2017-05-12 16:30:00 +1000

    Living room section 1 data

--- Software/main.js
+++ Software/main.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
+require('electron-debug')({showDevTools: true});
 
 app.on('ready', () => {
 	let win = new BrowserWindow({width:800, height: 600})
